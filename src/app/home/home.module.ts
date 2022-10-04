849d08d29d635430f6c8fe7e7420a060597153e9
JoseAntonioBenitez <benitezruiz.joseantonio@gmail.com>
2022-10-04 13:06:13 +0200

    Actualizacion Advina el numero v2

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
-
 import { HomePageRoutingModule } from './home-routing.module';
+import { HighlightDirective } from './directives/highlist.directive';
 
 
 @NgModule({
@@ -14,6 +14,6 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage,HighlightDirective]
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,30 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  randomNumber:number= Number((Math.random() * (100 - 0) + 0).toFixed(0));
+  estado_:number = 2;
+  num:number=0;
+
   constructor() {}
 
+  tryIt(num: number){
+    
+    if(this.randomNumber > num){
+      console.log(num);
+      console.log(this.randomNumber)
+      return this.estado_= 1;
+    }else if (this.randomNumber < num ){
+      console.log(num);
+      console.log(this.randomNumber)
+      return this.estado_=-1;
+    }
+
+    if(num == this.randomNumber){
+      return this.estado_=0;
+    }
+  }
+
+  restartIt(){
+    location.reload();
+  }
 }
